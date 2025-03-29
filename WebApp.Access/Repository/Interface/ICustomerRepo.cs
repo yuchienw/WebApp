42cb034da5dd8801f7f25d752fd235fa23df7914
Tim <yuchien.wang@icloud.com>
2025-03-29 18:21:15 +0800

    修正ICustomerRepo

--- WebApp.Access/Repository/Interface/ICustomerRepo.cs
+++ WebApp.Access/Repository/Interface/ICustomerRepo.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebApp.Access.Entity;
 
 namespace WebApp.Access.Repository.Interface
 {
-    public interface ICustomerRepo
+    public interface ICustomerRepo : IGenericRepo<Customer>
     {
     }
 }
